afcd7651e890a884fc51ad48f1f7e76782a77c12
txclab-student <txclab.student@gmail.com>
2025-08-26 16:37:13 -0400

    Added variables and provider block

--- variables.tf
+++ variables.tf
@@ -15,3 +15,14 @@ variable "repo_name" {
  description = "Unique identifier for a each student session and workspace"
  default     = "txclab-kei6"
 } 
+variable "hostname" {
+      type = string
+}
+
+variable "username" {
+      type = string
+}
+
+variable "password" {
+      type = string
+}
